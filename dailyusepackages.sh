8fb4346166a2e9b1533a84be8dc07cdfda0ba15d
Usman Musa <usmanmusa1920@gmail.com>
2022-09-21 01:28:55 +0100

    add some packages list

---
+++ dailyusepackages.sh
@@ -0,0 +1,52 @@
+"""
+    MY SYSTEM PACKAGES TO INSTALL
+
+    git
+    tmux
+    htop
+    ncdu
+    whois
+    banner
+    tree
+    mysql
+        pip install mysqlclient
+    apt install sqlite3
+    pkg install pipenv
+    pkg install sl
+    pkg install curl
+    pkg install nmap
+    pkg install zsh
+    ssh
+    man
+    sudo snap install --classic code
+    qiskit
+    numpy
+    matplotlib
+    requests
+    BeautifulSoup
+    lxml
+    html5lib
+    django
+    flask
+    sudo snap install --classic  heroku
+    sqlalchemy
+    sudo apt-get install apache2
+    apt udate
+    apt upgrade
+    nodejs
+    VS_code theme (Andromeda italic)
+        Dev c++
+        c++ (code runner)
+        c/c++ (select the first)
+    sudo apt install build-essential (c++ compiler)
+    python
+    seaborn
+    scipy
+    scikit learn
+    jupyter notebook
+        pip install jupyterthemes
+        jt -t chesterish (for black theme)
+        jt -r (for white theme)
+    
+
+"""
