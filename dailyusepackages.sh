0d62bb16da878a839745269ffd2b5225022ac5d5
Usman Musa <usmanmusa1920@gmail.com>
2022-09-21 03:38:29 +0100

    added some pakages to be install

--- dailyusepackages.sh
+++ dailyusepackages.sh
@@ -1,56 +1,83 @@
 """
-    MY SYSTEM PACKAGES TO INSTALL
+    *****MY SYSTEM PACKAGES TO INSTALL*****
 
-    git
-    tmux
-    htop
-    ncdu
-    whois
-    banner
-    tree
-    mysql
-        pip install mysqlclient
-    apt install sqlite3
-    pkg install pipenv
-    pkg install sl
-    pkg install curl
-    pkg install nmap
-    pkg install zsh
-    ssh
-    man
-    sudo snap install --classic code
-    qiskit
-    numpy
-    matplotlib
-    requests
-    BeautifulSoup
-    lxml
-    html5lib
-    django
-    flask
-    sudo snap install --classic  heroku
-    sqlalchemy
-    sudo apt-get install apache2
-    apt udate
-    apt upgrade
-    nodejs
-    VS_code theme (Andromeda italic)
-        Dev c++
-        c++ (code runner)
-        c/c++ (select the first)
-    sudo apt install build-essential (c++ compiler)
-    python
-    seaborn
-    scipy
-    scikit learn
-    jupyter notebook
-        pip install jupyterthemes
-        jt -t chesterish (for black theme)
-        jt -r (for white theme)
-    
-    kubernetes
-        sudo snap install --classic  kubectl
-    docker
-    ansible
+            pip install mysqlclient
+        apt install sqlite3
+        pkg install pipenv
+        pkg install sl
+        pkg install curl
+        pkg install nmap
+        pkg install zsh
+        ssh
+        man
+        
+        qiskit
+        numpy
+        matplotlib
+        requests
+        BeautifulSoup
+        lxml
+        html5lib
+        django
+        flask
+        
+        sqlalchemy
+        sudo apt-get install apache2
+        apt udate
+        apt upgrade
+        nodejs
+        VS_code theme (Andromeda italic)
+            Dev c++
+            c++ (code runner)
+            c/c++ (select the first)
+        sudo apt install build-essential (c++ compiler)
+        python
+        seaborn
+        scipy
+        scikit learn
+        jupyter notebook
+            pip install jupyterthemes
+            jt -t chesterish (for black theme)
+            jt -r (for white theme)
+        
+        kubernetes
+            sudo snap install --classic  kubectl
+        docker
+        ansible
 
 """
+#!/usr/bin/env bash
+
+# updating and upgrading system
+sudo apt-get update
+sudo apt-get upgrade
+
+
+# for my system
+sudo apt install ncdu
+sudo apt install htop
+sudo apt install tmux
+sudo apt install whois
+sudo apt install banner
+sudo apt install tree
+sudo apt install mysql
+
+
+# for python
+sudo apt-get install python3-pip
+sudo apt-get install python3-venv
+
+# for working with myql
+pip install mysqlclient
+
+
+# installing some pakages
+sudo snap install snap-store
+sudo snap install --classic code
+# sudo snap install --classic heroku
+# sudo snap install kubectl --classic
+
+
+# Install LinuxMint Applications
+sudo apt-get install google-chrome
+
